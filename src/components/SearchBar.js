a3cbf7332c963daa405cc6c71d7459b55ef12d99
ANAND VIBHUTI <mailaddress.anandvibhuti@gmail.com>
2023-07-04 21:35:48 +0530

    lenovo 7/4/2023

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,15 @@
-function SearchBar({onSubmit}){
-    const handleFormSubmit=(event)=>{
-        event.preventDefault();
-       // console.log("i need to tell the parent about some data");
-       onSubmit('cars');
-    }
-    return(
-        <div>
-            <form onSubmit={handleFormSubmit}>
-          <input />
-          </form>
-         
-            
-        </div>
-    );
+function SearchBar({ onSubmit }) {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // console.log("i need to tell the parent about some data");
+    onSubmit("cars");
+  };
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <input />
+      </form>
+    </div>
+  );
 }
 export default SearchBar;

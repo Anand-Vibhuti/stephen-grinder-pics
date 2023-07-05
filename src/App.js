1b149d44f18233e78d096c4b06c84d425464366a
Anand Vibhuti <mailaddress.anandvibhuti@gmail.com>
2023-07-05 22:34:53 +0530

    img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import {useState } from 'react';
 import SearchBar from "./components/SearchBar";
 import searchImages from "./api";
+import ImageList from './components/ImageList';
 function App() {
+  const [images,setImages]=useState([]);
   const handleSubmit = async (term) => {
-    await searchImages(term);
+    const result=await searchImages(term);
+    setImages(result);
   };
   return (
     <div>
       <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
     </div>
   );
 }
--- src/components/ImageShow.js
+++ src/components/ImageShow.js
@@ -1,4 +1,7 @@
-function ImageShow() {
-  return <div>ImageShow</div>;
+function ImageShow({image}) {
+  return ( <div><form >{image.alt_description}
+  {console.log(image)}
+  <img src={image.links.download} style={{width:95,height:85}} /> 
+ </form> </div>);
 }
 export default ImageShow;
